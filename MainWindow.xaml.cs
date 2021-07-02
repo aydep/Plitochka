4291cf4eae21d66fa70e5eaa691418121bf5825d
aydep <ayder2002@gmail.com>
2021-07-02 22:22:15 +0300

    asd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,21 +26,35 @@ namespace Plitochka
         }
 
         string focused_plit;
+        UIElement focused_plit_el;
 
         private void button_left_Click(object sender, RoutedEventArgs e)
         {
-            Canvas.SetLeft((UIElement)focused_plit, Canvas.GetLeft(plit1) - 40);
+            Canvas.SetLeft(focused_plit_el, Canvas.GetLeft(focused_plit_el) - 40);
         }
 
         private void button_right_Click(object sender, RoutedEventArgs e)
         {
-                Canvas.SetLeft(plit1, Canvas.GetLeft(plit1) + 40);
+            Canvas.SetLeft(focused_plit_el, Canvas.GetLeft(focused_plit_el) + 40);
         }
 
         public void Plit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             focused_plit = ((Rectangle)e.OriginalSource).Name;
             tehta.Text = focused_plit;
+            focused_plit_el = (UIElement)LogicalTreeHelper.FindLogicalNode(canv, focused_plit);
+        }
+
+        private void button_rotate_left_Click(object sender, RoutedEventArgs e)
+        {
+            Style stbt = new Style();
+            stbt.Setters.Add(new Setter { Property = Control.WidthProperty, Value = 100});
+            focused_plit.Style = stbt;
+        }
+
+        private void button_rotate_right_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
